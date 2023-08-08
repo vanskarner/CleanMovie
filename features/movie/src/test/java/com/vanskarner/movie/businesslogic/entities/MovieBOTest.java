b857c78a5e96bd1c3d0cc6d9545f424442811049
Vanskarner <vanskarner@gmail.com>
2023-08-07 19:41:03 -0500

    features - movie | test: creation method pattern and method renaming

--- features/movie/src/test/java/com/vanskarner/movie/businesslogic/entities/MovieBOTest.java
+++ features/movie/src/test/java/com/vanskarner/movie/businesslogic/entities/MovieBOTest.java
@@ -6,37 +6,50 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
 public class MovieBOTest {
+    static int counter = 0;
 
     @Test
-    public void calculateRecommendation_superiorVoteCountInferiorVoteAverage_false() {
+    public void calculateRecommendation_OnlyVoteAverageIsInferior_false() {
         int voteCount = 100;
         float voteAverage = 7.4f;
-        MovieBO movieBO = getMovieDetailBO(voteCount, voteAverage);
+        MovieBO movieBO = createMovie(voteCount, voteAverage);
 
         assertFalse(movieBO.calculateRecommendation());
     }
 
     @Test
-    public void calculateRecommendation_inferiorVoteCountSuperiorVoteAverage_false() {
+    public void calculateRecommendation_OnlyVoteCountIsInferior_false() {
         int voteCount = 74;
         float voteAverage = 10;
-        MovieBO movieBO = getMovieDetailBO(voteCount, voteAverage);
+        MovieBO movieBO = createMovie(voteCount, voteAverage);
 
         assertFalse(movieBO.calculateRecommendation());
     }
 
     @Test
-    public void calculateRecommendation_superiorVoteCountSuperiorVoteAverage_true() {
+    public void calculateRecommendation_voteCountAverageVoteAreSuperior_true() {
         int voteCount = 76;
         float voteAverage = 7.5f;
-        MovieBO movieBO = getMovieDetailBO(voteCount, voteAverage);
+        MovieBO movieBO = createMovie(voteCount, voteAverage);
 
         assertTrue(movieBO.calculateRecommendation());
     }
 
-    private MovieBO getMovieDetailBO(int voteCount, float voteAverage) {
-        return new MovieBO(0, "", "", "", voteCount, voteAverage,
-                "", "");
+    private MovieBO createMovie(int voteCount, float voteAverage) {
+        int uniqueId = getUniqueInt();
+        return new MovieBO(
+                uniqueId,
+                "Title" + uniqueId,
+                "Image" + uniqueId,
+                "BackgroundImage" + uniqueId,
+                voteCount,
+                voteAverage,
+                "ReleaseData" + uniqueId,
+                "Overview" + uniqueId);
     }
 
+    private int getUniqueInt() {
+        counter++;
+        return counter;
+    }
 }
