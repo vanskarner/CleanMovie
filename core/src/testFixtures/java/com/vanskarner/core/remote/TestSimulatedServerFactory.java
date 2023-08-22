0f4964bee12174928dc12fc45192f33c2d7a017d
Vanskarner <vanskarner@gmail.com>
2023-08-22 01:20:57 -0500

    renamed

--- core/src/testFixtures/java/com/vanskarner/core/remote/TestSimulatedServerFactory.java
+++ core/src/testFixtures/java/com/vanskarner/core/remote/TestSimulatedServerFactory.java
@@ -1,6 +1,6 @@
 package com.vanskarner.core.remote;
 
-public final class SimulatedServerFactory {
+public final class TestSimulatedServerFactory {
 
     public static TestSimulatedServer create(Class<?> testClass) {
         return new MockSimulatedServer(testClass);
