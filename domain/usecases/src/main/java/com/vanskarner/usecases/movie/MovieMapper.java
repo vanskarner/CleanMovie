c0077e1a73c2cfc27cf2a9bd2437e9816d8e8d06
Vanskarner <vanskarner@gmail.com>
2023-09-26 16:22:58 -0500

    solution to: Avoid instantiating new objects inside loops

--- domain/usecases/src/main/java/com/vanskarner/usecases/movie/MovieMapper.java
+++ domain/usecases/src/main/java/com/vanskarner/usecases/movie/MovieMapper.java
@@ -40,8 +40,12 @@ class MovieMapper {
     public static MoviesDS convert(List<MovieBO> movieBO) {
         List<MovieDS> list = new ArrayList<>();
         for (MovieBO item : movieBO)
-            list.add(new MovieDS(item.getId(), item.getTitle(), item.getImage()));
+            list.add(toDS(item));
         return new MoviesDS(list);
+    }
+
+    private static MovieDS toDS(MovieBO item) {
+        return new MovieDS(item.getId(), item.getTitle(), item.getImage());
     }
 
 }
