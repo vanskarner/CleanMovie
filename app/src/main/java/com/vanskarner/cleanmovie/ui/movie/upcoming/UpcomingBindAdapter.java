4ea5fbc2b8fbc6b511fe9bc0f64be739c47abc27
Vanskarner <vanskarner@gmail.com>
2023-09-28 18:12:34 -0500

    solution to: Avoid instantiation through private constructors from outside of the constructors class.

--- app/src/main/java/com/vanskarner/cleanmovie/ui/movie/upcoming/UpcomingBindAdapter.java
+++ app/src/main/java/com/vanskarner/cleanmovie/ui/movie/upcoming/UpcomingBindAdapter.java
@@ -41,7 +41,7 @@ class UpcomingBindAdapter implements BindAdapter<MovieBasicModel, UpcomingBindAd
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, LayoutInflater inflater) {
         UpcomingItemBinding binding = UpcomingItemBinding
                 .inflate(inflater, parent, false);
-        return new ViewHolder(binding, onClickItem);
+        return ViewHolder.create(binding,onClickItem);
     }
 
     static class ViewHolder extends RecyclerView.ViewHolder {
@@ -54,6 +54,12 @@ class UpcomingBindAdapter implements BindAdapter<MovieBasicModel, UpcomingBindAd
             super.itemView.setTag(this);
             super.itemView.setOnClickListener(onClickListener);
         }
+
+        private static ViewHolder create(UpcomingItemBinding binding,
+                                         View.OnClickListener onClickItem) {
+            return new ViewHolder(binding, onClickItem);
+        }
+
     }
 
 }
