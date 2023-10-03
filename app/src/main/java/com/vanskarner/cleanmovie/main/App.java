b5fc7c678d8eaa11eb1b02195bfe4a5e602e96df
Vanskarner <vanskarner@gmail.com>
2023-10-03 17:17:45 -0500

    Solution to: Unnecessary Code

--- app/src/main/java/com/vanskarner/cleanmovie/main/App.java
+++ app/src/main/java/com/vanskarner/cleanmovie/main/App.java
@@ -8,11 +8,6 @@ import dagger.android.support.DaggerApplication;
 public class App extends DaggerApplication {
 
     @Override
-    public void onCreate() {
-        super.onCreate();
-    }
-
-    @Override
     protected AndroidInjector<? extends DaggerApplication> applicationInjector() {
         return DaggerAppComponent.factory().create(this);
     }
--- app/src/main/java/com/vanskarner/cleanmovie/ui/movie/upcoming/Pagination.java
+++ app/src/main/java/com/vanskarner/cleanmovie/ui/movie/upcoming/Pagination.java
@@ -4,7 +4,6 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
 import com.vanskarner.cleanmovie.main.di.scopes.PerFragment;
 
@@ -12,7 +11,6 @@ import javax.inject.Inject;
 
 @PerFragment
 class Pagination extends RecyclerView.OnScrollListener {
-
     public static final int LAST_POSITION = 1;
     public static final int LAST_POSITION_COMPLETE = 2;
 
@@ -70,17 +68,16 @@ class Pagination extends RecyclerView.OnScrollListener {
     }
 
     private boolean isLastPosition(RecyclerView.LayoutManager manager) {
-        return (getLastPosition(manager) == manager.getItemCount() - 1);
+        return getLastPosition(manager) == manager.getItemCount() - 1;
     }
 
     private int getLastPosition(@NonNull RecyclerView.LayoutManager layoutManager) {
+        int position = 0;
         if (layoutManager instanceof GridLayoutManager)
-            return lastPositionGrid((GridLayoutManager) layoutManager);
+            position = lastPositionGrid((GridLayoutManager) layoutManager);
         else if (layoutManager instanceof LinearLayoutManager)
-            return lastPositionLinear((LinearLayoutManager) layoutManager);
-        else if (layoutManager instanceof StaggeredGridLayoutManager)
-            return lastPositionStaggered((StaggeredGridLayoutManager) layoutManager);
-        return 0;
+            position = lastPositionLinear((LinearLayoutManager) layoutManager);
+        return position;
     }
 
     private int lastPositionGrid(@NonNull GridLayoutManager manager) {
@@ -93,16 +90,6 @@ class Pagination extends RecyclerView.OnScrollListener {
         return (positionType == LAST_POSITION) ?
                 manager.findLastVisibleItemPosition() :
                 manager.findLastCompletelyVisibleItemPosition();
-    }
-
-    private int lastPositionStaggered(@NonNull StaggeredGridLayoutManager manager) {
-        int[] lastPositions = (positionType == LAST_POSITION) ?
-                manager.findLastVisibleItemPositions(null) :
-                manager.findLastCompletelyVisibleItemPositions(null);
-        if (lastPositions == null || lastPositions.length <= 0) return 0;
-        int max = lastPositions[0];
-        for (int value : lastPositions) max = Math.max(max, value);
-        return max;
     }
 
 }
