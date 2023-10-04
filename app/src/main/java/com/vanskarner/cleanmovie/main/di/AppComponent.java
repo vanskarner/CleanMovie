f583fb2e4affb74e98df2813c3f7a54e745fa746
Vanskarner <vanskarner@gmail.com>
2023-10-04 00:30:15 -0500

    updated Main

--- app/src/main/java/com/vanskarner/cleanmovie/main/di/AppComponent.java
+++ app/src/main/java/com/vanskarner/cleanmovie/main/di/AppComponent.java
@@ -5,7 +5,7 @@ import android.content.Context;
 import com.vanskarner.cleanmovie.main.App;
 import com.vanskarner.cleanmovie.main.db.RoomDBModule;
 import com.vanskarner.cleanmovie.main.di.modules.ProjectParametersModule;
-import com.vanskarner.cleanmovie.main.di.modules.ViewModule;
+import com.vanskarner.cleanmovie.main.di.modules.UIModule;
 import com.vanskarner.core.main.CoreModule;
 import com.vanskarner.movie.main.MovieModule;
 
@@ -22,7 +22,7 @@ import dagger.android.AndroidInjector;
         RoomDBModule.class,
         CoreModule.class,
         MovieModule.class,
-        ViewModule.class
+        UIModule.class
 })
 public interface AppComponent extends AndroidInjector<App> {
 
