864ca606e87a2f0c781ed491daeb572450efac56
Vanskarner <vanskarner@gmail.com>
2023-10-09 18:09:02 -0500

    Creation of a map for use cases to avoid multiple parameters and to be called through a filter.

--- features/movie/src/main/java/com/vanskarner/movie/businesslogic/MovieBusinessLogicModule.java
+++ features/movie/src/main/java/com/vanskarner/movie/businesslogic/MovieBusinessLogicModule.java
@@ -4,6 +4,8 @@ import javax.inject.Singleton;
 
 import dagger.Binds;
 import dagger.Module;
+import dagger.multibindings.ClassKey;
+import dagger.multibindings.IntoMap;
 
 /** @noinspection unused*/
 @Module
@@ -13,4 +15,50 @@ public abstract class MovieBusinessLogicModule {
     @Singleton
     abstract MovieServices bindMovieServices(MovieDefaultServices movieDefaultServices);
 
+    @Binds
+    @IntoMap
+    @ClassKey(CheckFavoriteMovieUseCase.class)
+    public abstract UseCase<?,?> bindCheckFavoriteMovieUseCase(CheckFavoriteMovieUseCase useCase);
+
+    @Binds
+    @IntoMap
+    @ClassKey(DeleteAllFavoriteMoviesUseCase.class)
+    public abstract UseCase<?,?>
+    bindDeleteAllFavoriteMoviesUseCase(DeleteAllFavoriteMoviesUseCase useCase);
+
+    @Binds
+    @IntoMap
+    @ClassKey(FilterUpcomingMoviesUseCase.class)
+    public abstract UseCase<?,?>
+    bindFilterUpcomingMoviesUseCase(FilterUpcomingMoviesUseCase useCase);
+
+    @Binds
+    @IntoMap
+    @ClassKey(FindFavoriteMovieUseCase.class)
+    public abstract UseCase<?,?>
+    bindFindFavoriteMovieUseCase(FindFavoriteMovieUseCase useCase);
+
+    @Binds
+    @IntoMap
+    @ClassKey(FindUpcomingMovieUseCase.class)
+    public abstract UseCase<?,?>
+    bindFindUpcomingMovieUseCase(FindUpcomingMovieUseCase useCase);
+
+    @Binds
+    @IntoMap
+    @ClassKey(ShowFavoriteMoviesUseCase.class)
+    public abstract UseCase<?,?>
+    bindShowFavoriteMoviesUseCase(ShowFavoriteMoviesUseCase useCase);
+
+    @Binds
+    @IntoMap
+    @ClassKey(ShowUpcomingMoviesUseCase.class)
+    public abstract UseCase<?,?>
+    bindShowUpcomingMoviesUseCase(ShowUpcomingMoviesUseCase useCase);
+
+    @Binds
+    @IntoMap
+    @ClassKey(ToggleMovieFavoriteUseCase.class)
+    public abstract UseCase<?,?>
+    bindToggleMovieFavoriteUseCase(ToggleMovieFavoriteUseCase useCase);
 }
